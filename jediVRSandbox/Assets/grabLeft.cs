db40d176af15d22098018a1d184af02309c8bb5f
HunterFinney <70294732+HunterFinney@users.noreply.github.com>
2020-10-26 18:28:39 -0600

    added Grab on hand triggers
    no rotations just position right now

---
+++ jediVRSandbox/Assets/grabLeft.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grabLeft : MonoBehaviour
+{
+    public GameObject hand;
+    private bool canGrab;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.Get(OVRInput.RawButton.LHandTrigger))
+        {
+            canGrab = true;
+        }
+        else
+        {
+            canGrab = false;
+        }
+
+    }
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log("grab");
+        if (other.gameObject.tag == "grab")
+        {
+            if (canGrab)
+            {
+                other.gameObject.transform.position = new Vector3(hand.transform.position.x, hand.transform.position.y, hand.transform.position.z);
+                other.gameObject.transform.eulerAngles = new Vector3(hand.transform.rotation.x, hand.transform.rotation.y, hand.transform.rotation.z);
+            }
+
+        }
+    }
+}
