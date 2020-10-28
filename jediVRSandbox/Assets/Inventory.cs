06053426e81a467f63487d924a91c6175f41af19
RBerghout <rachelberghout@mail.weber.edu>
2020-10-28 12:03:45 -0600

    Added Key/Lock Puzzle and Inventory System

---
+++ jediVRSandbox/Assets/Inventory.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+//Script based on various tutorials and code 
+// from Unity Documentation and Unity Forums
+public class Inventory : MonoBehaviour
+{
+	public GameObject inventoryUI;
+	private List<GameObject> inventoryList;
+	private bool showUI = false;
+	private GameObject inventoryPanel;
+	
+    void Start()
+    {
+        if(inventoryList == null){
+			inventoryList = new List<GameObject>();
+		}
+		
+		if(inventoryUI){
+			showUI = true;
+			foreach(Transform child in transform){
+				if(child.name == "InventoryUI")
+					inventoryPanel = child.transform.GetChild(0).gameObject;
+			}
+		}
+    }
+	
+    void Update()
+    {
+		if(Input.GetKeyDown("i") || (OVRInput.Get(OVRInput.RawButton.X))){
+			//show inventory
+			showUI = !showUI;
+		}
+		showInventoryUI();
+    }
+	
+	void showInventoryUI(){
+		if(inventoryUI){
+			inventoryUI.SetActive(showUI);
+			
+			if(showUI){
+				//show objects in inventory
+				int indx = 0;
+				Debug.Log(inventoryPanel);
+				//foreach(Transform slot in inventoryPanel.transform){
+				for(int i = 0; i<inventoryPanel.transform.childCount; i++){
+					GameObject itemImage = inventoryPanel.transform.GetChild(i).GetChild(0).gameObject;
+					GameObject text = inventoryPanel.transform.GetChild(i).GetChild(1).gameObject;
+										
+					if(indx < inventoryList.Count){
+						GameObject slotItem = inventoryList[i].gameObject;
+						//Set image and name for item in inventory
+						itemImage.GetComponent<Image>().sprite = slotItem.GetComponent<FindItem>().itemIcon;
+						itemImage.GetComponent<Image>().color = new Color(0,0,0,1);
+
+						text.GetComponent<Text>().text = slotItem.name;
+					}
+					else{
+						//make them blank
+						itemImage.GetComponent<Image>().sprite = null;
+						itemImage.GetComponent<Image>().color = new Color(0,0,0,0);
+						text.GetComponent<Text>().text = "";
+					}
+				}
+			}
+		}
+	}
+	
+	public void AddItem(GameObject obj){
+		inventoryList.Add(obj);
+		obj.SetActive(false);
+	}
+	
+	public bool HasItem(GameObject obj, bool removeUsed){
+		if(inventoryList.Contains(obj)){
+			if(removeUsed){
+				UseItem(obj);
+			}
+			return true;
+		}
+		else
+			return false;
+	}
+	
+	private void UseItem(GameObject obj){
+		inventoryList.Remove(obj);
+	}
+}
---
+++ jediVRSandbox/Assets/UnlockItem.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Script based on various tutorials and code 
+// from Unity Documentation and Unity Forums
+public class UnlockItem : MonoBehaviour
+{
+	public GameObject player; //Input the player object
+	public GameObject key; //Input the key object
+	public int DistanceFromObjectRadius = 5;
+	private GameObject lid;
+	private GameObject hinge;
+	private bool open = false;
+	private Vector3 hitRadius;
+	private Inventory inventory;
+
+    void Start()
+    {
+		if(name == "box"){
+			//https://answers.unity.com/questions/183649/how-to-find-a-child-gameobject-by-name.html
+			foreach(Transform child in transform){
+				if(child.name == "lid")
+					lid = child.gameObject;
+				if(child.name == "hinge"){
+					hinge = child.gameObject;
+				}
+			}
+		}
+		
+		hitRadius = new Vector3(DistanceFromObjectRadius, DistanceFromObjectRadius,DistanceFromObjectRadius);
+		
+		inventory = player.GetComponent<Inventory>();
+    }
+	
+	void Update(){
+		if(OVRInput.Get(OVRInput.RawButton.B)){
+			OnMouseDown();
+		}
+	}
+	
+	void OnMouseDown(){
+		if(inventory.HasItem(key, true)){		
+			//https://answers.unity.com/questions/1699266/how-do-you-check-if-a-game-object-is-in-the-radius.html
+			if(Vector3.Distance(transform.position, player.transform.position) < 5 && !open){
+				open = true;
+				
+			   //rotate lid
+			   lid.transform.Translate(hinge.transform.localPosition);
+			   lid.transform.Rotate(90, 0, 0);
+			}
+		}
+	}
+}
