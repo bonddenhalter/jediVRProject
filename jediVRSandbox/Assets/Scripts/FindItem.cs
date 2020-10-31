673a45a29d3c2b6263dffb4e10fd2bad7813adf5
RBerghout <rachelberghout@mail.weber.edu>
2020-10-30 21:08:11 -0600

    Added Key puzzle and inventory

---
+++ jediVRSandbox/Assets/Scripts/FindItem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Script based on various tutorials and code 
+// from Unity Documentation and Unity Forums
+public class FindItem : MonoBehaviour
+{
+	public Sprite itemIcon; //input image
+	private bool found = false;
+	private Inventory inventory;
+		
+	void Start(){
+		
+	}
+	
+	void OnTriggerEnter(Collider col){
+		if(col.gameObject.name == "OVRPlayerController" && !found){
+			found = true;
+			
+			//Put object into player's inventory
+			inventory = col.gameObject.GetComponent<Inventory>();
+			GameObject item = this.gameObject;
+			inventory.AddItem(item);
+		}
+	}		
+}
