0ea7fc45286a627586722c377dd80fdee0a539a9
HunterFinney <70294732+HunterFinney@users.noreply.github.com>
2020-11-01 11:22:34 -0700

    Added Ringing bell and Data Collection

---
+++ jediVRSandbox/Assets/BellSound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BellSound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.rotation.eulerAngles.x > 10f || this.transform.rotation.eulerAngles.x < -10f || this.transform.rotation.eulerAngles.z < -10f || this.transform.rotation.eulerAngles.z > 10f)
+        {
+            this.GetComponent<AudioSource>().Play();
+        }
+    }
+}
---
+++ jediVRSandbox/Assets/DataCollector.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEditor;
+
+public class DataCollector : MonoBehaviour
+{
+    public GameObject player;
+    private string dataBig;
+    private string dataSmall;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.GetComponent<ShrinkGrow>().shrinkPlayer)
+        {
+            dataBig = player.transform.position.x + player.transform.position.z + Time.deltaTime + "";
+        }
+        else
+        {
+            dataSmall = player.transform.position.x + player.transform.position.z + Time.deltaTime + "";
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            WriteString();
+        }
+    }
+
+    void WriteString()
+    {
+        string path = "Assets/test.txt";
+        StreamWriter writer = new StreamWriter(path, true);
+        writer.WriteLine(dataBig);
+        writer.WriteLine(dataSmall);
+        writer.Close();
+    }
+}
